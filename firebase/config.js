4db3d595a0e84e5ba3b45bff71d46b08503c1a5c
unknown <ericking2501@gmail.com>
2020-07-07 01:08:29 -0600

    v1.0

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA45NuuUwoWxVBsftIKL1kIEGQ6QK_DGIo",
+    authDomain: "restaurant-4af20.firebaseapp.com",
+    databaseURL: "https://restaurant-4af20.firebaseio.com",
+    projectId: "restaurant-4af20",
+    storageBucket: "restaurant-4af20.appspot.com",
+    messagingSenderId: "157118994987",
+    appId: "1:157118994987:web:b68cb1df8dc7d35b40a86e",
+    measurementId: "G-XPW7D3V6GL"
+  };
+
+  export default firebaseConfig;
